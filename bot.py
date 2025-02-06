2db8d83aeeea5ef75f547b242432d8ad78723cb1
Alexander Beley <beleysasha@gmail.com>
2025-02-07 09:46:42 +1200

    request_dates function switched to sync

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import logging
 import asyncio
 import random
 from time import sleep
+from datetime import datetime
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
@@ -22,9 +23,10 @@ config = load_config('.env')
 
 BOT_TOKEN = config.tg_bot.token
 LOG_FILE = 'chukotabia-bot-2.log'
-REQUEST_INTERVAL = 180
+REQUEST_INTERVAL = 120
 EVERYDAY_MESSAGE_HOUR = 3
 EVERYDAY_MESSAGE_MINUTE = 0
+ADMIN_CHAT_ID = os.getenv('ADMIN_CHAT_ID')
 
 
 scheduler = AsyncIOScheduler()
@@ -64,7 +66,7 @@ async def send_message_to_users(dp: Dispatcher):
 
 async def request_dates(dp: Dispatcher):
     interval = random.randint(55, 165)
-    interval = 10
+    interval = 5
     users = get_all_users()
     for user in users:
         user_tickets = get_tickets(user.tg_id)
@@ -77,11 +79,33 @@ async def request_dates(dp: Dispatcher):
         # sleep(interval)
     
 
+def request_dates_sync(dp: Dispatcher):
+    print(f'# # # # # # # Requesting tickets of all users')
+    interval = random.randint(55, 165)
+    interval = 5
+    users = get_all_users()
+    for user in users:
+        print(f'% % % % % % % Requesting tickets of user {user.tg_id} {user.username}')
+        user_tickets = get_tickets(user.tg_id)
+        for user_ticket in user_tickets:
+            print(f'~ ~ ~ ~ ~ ~ ~ Requesting user {user_ticket.date} {user_ticket.direction}')
+            result, ticket_message = request_tickets(user_ticket.date, user_ticket.direction)
+            if result:
+                bot.send_message(chat_id=user.tg_id, text=ticket_message)
+            elif user.tg_id == ADMIN_CHAT_ID:
+                now = datetime.now()
+                current_datetime = now.strftime("%Y-%m-%d %H:%M:%S")
+                ticket_message = f'Это отладочное сообщение. Билетов нет. {current_datetime}'
+                bot.send_message(chat_id=user.tg_id, text=ticket_message)
+            sleep(interval)
+        sleep(interval)
+    
+
 async def schedule_jobs():
     scheduler.add_job(
         send_message_to_users, 'cron', day_of_week='*',
         hour=EVERYDAY_MESSAGE_HOUR, minute=EVERYDAY_MESSAGE_MINUTE, end_date='2030-12-31' , args=(dp,))
-    scheduler.add_job(request_dates, 'interval', seconds = REQUEST_INTERVAL, args=(dp,), max_instances=2)
+    scheduler.add_job(request_dates_sync, 'interval', seconds = REQUEST_INTERVAL, args=(dp,), max_instances=2)
 
 
 if __name__ == '__main__':
